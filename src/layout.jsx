57c022fc1d2ad065977fd7071d1033ed7dbfb18d
silentboy <silent32boy88@gmail.com>
2026-03-16 17:45:21 +0530

    NavBar Updated

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,8 +1,8 @@
 
 
 import React from "react";
-import Footer from "./components/Footer/footer";
-import Header from "./components/Header/header";
+import Footer from "./components/Footer/footer.jsx";
+import Header from "./components/Header/header.jsx";
 import { Outlet } from "react-router-dom";
 
 function Layout(){

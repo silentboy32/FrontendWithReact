9fdcc5f56ab3d4094a20586dffc9b6dd5059dfa0
silentboy <silent32boy88@gmail.com>
2026-03-16 18:10:38 +0530

    navlink added .

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -31,13 +31,13 @@ export default function Header() {
                         </Link>
                     </div>
                     <div
-                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        className="hidden justify-between items-center w-full sm:flex lg:w-auto lg:order-1"
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                to="/"
+                                    to="/"
                                     className={({ isActive }) =>
                                         ` ${isActive ? " text-blue-600" : " text-red-500"} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }

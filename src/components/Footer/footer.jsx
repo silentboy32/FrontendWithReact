0d8c8c3982561eaf694e64c74da46f2bec63a484
silentboy <silent32boy88@gmail.com>
2026-04-09 07:42:36 +0530

    updated date

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -93,7 +93,7 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2026
                         <a href="https:github.com/silentboy32/" className="hover:underline">
                             Dk Barman
                         </a>
